ee09b18ce1e0f97b16e5ca787fd12966d29ccde9
David Christie <davidchristie.nz@gmail.com>
2017-02-27 17:00:28 +1300

    Build into /docs

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '/build/'
+    path: path.resolve(__dirname, 'docs'),
+    publicPath: '/docs/'
   }
 }
